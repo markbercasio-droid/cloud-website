88f3a34c9801bb193091f55c2ce7d28f7367a276
markbercasio-droid <mark.bercasio@cdsp.edu.ph>
2025-08-28 21:07:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btn");
+  if (btn) {
+    btn.addEventListener("click", () => {
+      alert("This website is created by: Mark Bercasio from Bsit 1-21\n student number: 24-4032-51");
+    });
+  }
+});
